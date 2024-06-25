4b176c11a1c1d5fa44f527501e0493473bfc9e63
Juan Antonio Uceda Luque <juceluq@g.educaand.es>
2024-06-25 16:35:32 +0200

    Add: Calendar, Header, Footer.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,45 +1,46 @@
-import './bootstrap';
+import "./bootstrap";
 
-import 'bootstrap';
+import "bootstrap";
 
-import '@popperjs/core';
+import "@popperjs/core";
+document.addEventListener("DOMContentLoaded", function (event) {
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId);
 
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener("click", () => {
+                // show navbar
+                nav.classList.toggle("show");
+                // change icon
+                toggle.classList.toggle("bx-x");
+                // add padding to body
+                bodypd.classList.toggle("body-pd");
+                // add padding to header
+                headerpd.classList.toggle("body-pd");
 
-document.addEventListener("DOMContentLoaded", function(event) {
-   
-    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
-    bodypd = document.getElementById(bodyId),
-    headerpd = document.getElementById(headerId)
-    
-    // Validate that all variables exist
-    if(toggle && nav && bodypd && headerpd){
-    toggle.addEventListener('click', ()=>{
-    // show navbar
-    nav.classList.toggle('show')
-    // change icon
-    toggle.classList.toggle('bx-x')
-    // add padding to body
-    bodypd.classList.toggle('body-pd')
-    // add padding to header
-    headerpd.classList.toggle('body-pd')
-    })
-    }
-    }
-    
-    showNavbar('header-toggle','nav-bar','body-pd','header')
-    
+                // toggle hide-elements class
+                nav.classList.toggle("hide-elements");
+            });
+        }
+    };
+
+    showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+
     /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
-    
-    function colorLink(){
-    if(linkColor){
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
+    const linkColor = document.querySelectorAll(".nav_link");
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach((l) => l.classList.remove("active"));
+            this.classList.add("active");
+        }
     }
-    }
-    linkColor.forEach(l=> l.addEventListener('click', colorLink))
-    
-     // Your code to run since DOM is loaded and ready
-    });
+    linkColor.forEach((l) => l.addEventListener("click", colorLink));
+});
+
+import Calendar from 'js-year-calendar';
+import 'js-year-calendar/dist/js-year-calendar.css';
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -8,6 +8,8 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://unpkg.com/js-year-calendar@latest/dist/js-year-calendar.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 
@@ -19,36 +21,51 @@
 <body id="body-pd">
     <header class="header" id="header">
         <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-        <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
     </header>
-    <div class="l-navbar" id="nav-bar">
+    <hr class="hr">
+    <div class="l-navbar hide-elements" id="nav-bar">
         <nav class="nav">
-            <div> <a href="#" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span
-                        class="nav_logo-name">PRUEBA TÉCNICA</span> </a>
+            <div>
+                <a href="#" class="nav_logo">
+                    <i class='bx bx-layer nav_logo-icon'></i>
+                    <span class="nav_logo-name">PRUEBA TÉCNICA</span>
+                </a>
                 <li>
-                    <hr class="dropdown-divider">
+                    <hr class="dropdown-divider custom-divider">
                 </li>
-                <span class="nav_name">SOLUCIONES INFORMÁTICAS MJ</span>
+                <span class="nav_name styled-text">SOLUCIONES INFORMÁTICAS MJ</span>
                 <li>
-                    <hr class="dropdown-divider">
+                    <hr class="dropdown-divider custom-divider">
                 </li>
 
                 <div class="nav_list">
                     <a href="#" class="nav_link active">
-                        <i class='bx bx-grid-alt nav_icon'></i>
+                        <i class='bx bx-home nav_icon'></i>
                         <span class="nav_name">Inicio</span>
                     </a>
                     <a href="#" class="nav_link">
-                        <i class='bx bx-message-square-detail nav_icon'></i>
+                        <i class='bx bx-calendar nav_icon'></i>
                         <span class="nav_name">Días Festivos</span>
                     </a>
                     <a href="#" class="nav_link">
-                        <i class='bx bx-user nav_icon'></i>
+                        <i class='bx bx-group nav_icon'></i>
                         <span class="nav_name">Usuarios</span>
+                    </a>
                 </div>
             </div>
         </nav>
     </div>
+
+    <div class="container">
+        {{ $slot }}
+    </div>
+
+    <footer class="footer pt-3 bg-light fixed-bottom z-0 shadow-lg" id="footer">
+        <div class="container text-center">
+            <p>&copy; 2024 <strong>Soluciones Informáticas MJ, S.C.A</strong></p>
+        </div>
+    </footer>
+    
 </body>
 
 </html>
